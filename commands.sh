69a44adce3e716e5e16b4e658c648ec474383e9a
Hardip Patel <patelhardip@gmail.com>
2019-04-18 15:28:22 +1000

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,1 +1,12 @@
 
+#Download CRAM and .crai files.
+
+for i in `cut -f1 1000genomes.high_coverage.GRCh38DH.alignment.index | cut -f3- -d '/'`;
+do
+  lftp -e "pget -n24 ~/$i;quit" ftp.1000genomes.ebi.ac.uk;
+done
+
+for i in `cut -f3 1000genomes.high_coverage.GRCh38DH.alignment.index | cut -f3- -d '/'`;
+do
+  lftp -e "pget -n24 ~/$i;quit" ftp.1000genomes.ebi.ac.uk;
+done
