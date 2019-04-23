36b7502ee13431d57f1be7273a9c32eba4956884
Hardip Patel <patelhardip@gmail.com>
2019-04-23 15:36:32 +1000

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -10,3 +10,12 @@ for i in `cut -f3 1000genomes.high_coverage.GRCh38DH.alignment.index | cut -f3- 
 do
   lftp -e "pget -n24 ~/$i;quit" ftp.1000genomes.ebi.ac.uk;
 done
+
+#Download reference genome file
+wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa
+
+#For female samples, calculate reads that map to the Y chromosome
+for i in *.cram; 
+do 
+  echo `basename $i | cut -f1 -d '.'`, `samtools view --reference GRCh38_full_analysis_set_plus_decoy_hla.fa $i chrY | wc -l`;
+done
